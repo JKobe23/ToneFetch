dce2889938ce9572f99c2b4e84f79dd90f3ca07e
JKobe23 <93482070+JKobe23@users.noreply.github.com>
2021-12-05 22:48:26 +0200

    Login and signup done

--- app/src/main/java/com/lau/kobstonefetch/LoginActivity.java
+++ app/src/main/java/com/lau/kobstonefetch/LoginActivity.java
@@ -1,40 +1,106 @@
 package com.lau.kobstonefetch;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.cardview.widget.CardView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class LoginActivity extends AppCompatActivity {
 
-    EditText email, pass;
+    EditText username, pass;
     CardView btn;
     TextView reg;
+
+    DatabaseReference dbr = FirebaseDatabase.getInstance().getReferenceFromUrl("https://tonefetch-default-rtdb.firebaseio.com/");
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
 
-        email = (EditText) findViewById(R.id.login_username);
-        pass = (EditText) findViewById(R.id.login_pass);
+        username = (EditText) findViewById(R.id.username);
+        pass = (EditText) findViewById(R.id.passwordd);
         btn = (CardView) findViewById(R.id.login_card);
         reg = (TextView) findViewById(R.id.register_text);
     }
 
     public void UserLogin (View view) {
 
-        final String emailTxt = email.getText().toString();
+        final String userTxt = username.getText().toString();
         final String passTxt = pass.getText().toString();
 
-        if(emailTxt.isEmpty() || passTxt.isEmpty()) Toast.makeText(this, "Please fill in the required fields!",Toast.LENGTH_SHORT).show();
+        if(userTxt.isEmpty() || passTxt.isEmpty()) Toast.makeText(this, "Please fill in the required fields!",Toast.LENGTH_SHORT).show();
+
         else {
+
+            dbr.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                    if(snapshot.hasChild(userTxt)) {
+
+                        String password_entry = snapshot.child(userTxt).child("password").getValue(String.class);
+                        String hashed_entry = encryptPass(password_entry);
+                        String stored_hash = encryptPass(passTxt);
+                        if (hashed_entry.equals(stored_hash)) Toast.makeText(LoginActivity.this, "Login successful.",Toast.LENGTH_SHORT).show();
+                        else Toast.makeText(LoginActivity.this, "Wrong password, try again.",Toast.LENGTH_SHORT).show();
+                    }
+                    else Toast.makeText(LoginActivity.this, "Invalid username", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
 
         }
     }
+
+    public void UserSignup (View view) {
+        Intent i1 = new Intent(getApplicationContext(), RegisterActivity.class);
+        startActivity(i1);
+
+    }
+
+    private String encryptPass(String original) {
+
+        try {
+
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            byte[] messageDigest = md.digest(original.getBytes());
+            BigInteger no = new BigInteger(1, messageDigest);
+            String hashtext = no.toString(16);
+
+
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+
+            return hashtext;
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
+
+
--- app/src/main/java/com/lau/kobstonefetch/RegisterActivity.java
+++ app/src/main/java/com/lau/kobstonefetch/RegisterActivity.java
@@ -1,14 +1,97 @@
 package com.lau.kobstonefetch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText new_user;
+    EditText new_pass;
+    CardView regBtn;
+
+    DatabaseReference dbr = FirebaseDatabase.getInstance().getReferenceFromUrl("https://tonefetch-default-rtdb.firebaseio.com/");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
+        new_user = (EditText) findViewById(R.id.new_user);
+        new_pass = (EditText) findViewById(R.id.new_pass);
+        regBtn = (CardView) findViewById(R.id.reg_card);
+    }
+
+    public void SignUp(View view) {
+
+        final String userTxt = new_user.getText().toString();
+        final String passTxt = new_pass.getText().toString();
+
+
+        if (userTxt.isEmpty() || passTxt.isEmpty())
+            Toast.makeText(this, "Please fill in the required fields!", Toast.LENGTH_SHORT).show();
+
+        else {
+
+            String hashedTxt = encryptPass(passTxt);
+            dbr.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                    if (snapshot.hasChild(userTxt)) Toast.makeText(RegisterActivity.this, "Username already exists", Toast.LENGTH_SHORT).show();
+                    else {
+                        dbr.child("users").child(userTxt).child("password").setValue(hashedTxt);
+                        Toast.makeText(RegisterActivity.this, "Welcome to ToneFetch!",Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
+    }
+
+    private String encryptPass(String original) {
+
+        try {
+
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            byte[] messageDigest = md.digest(original.getBytes());
+            BigInteger no = new BigInteger(1, messageDigest);
+            String hashtext = no.toString(16);
+
+
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+
+            return hashtext;
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
