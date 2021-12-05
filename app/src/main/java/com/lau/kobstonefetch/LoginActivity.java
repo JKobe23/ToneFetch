1c1daa30735f30b4b90c633ecfb82dfc30076461
JKobe23 <93482070+JKobe23@users.noreply.github.com>
2021-12-05 14:49:24 +0200

    Firebase ready

--- app/src/main/java/com/lau/kobstonefetch/LoginActivity.java
+++ app/src/main/java/com/lau/kobstonefetch/LoginActivity.java
@@ -1,15 +1,40 @@
 package com.lau.kobstonefetch;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.cardview.widget.CardView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText email, pass;
+    CardView btn;
+    TextView reg;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
+        email = (EditText) findViewById(R.id.login_username);
+        pass = (EditText) findViewById(R.id.login_pass);
+        btn = (CardView) findViewById(R.id.login_card);
+        reg = (TextView) findViewById(R.id.register_text);
+    }
+
+    public void UserLogin (View view) {
+
+        final String emailTxt = email.getText().toString();
+        final String passTxt = pass.getText().toString();
+
+        if(emailTxt.isEmpty() || passTxt.isEmpty()) Toast.makeText(this, "Please fill in the required fields!",Toast.LENGTH_SHORT).show();
+        else {
+
+        }
     }
 }
